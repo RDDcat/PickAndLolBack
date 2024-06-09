e50d74c51b75dd32c44a7b44a981bb6ddc378e5d
RDDcat <yukmaro@gmail.com>
2024-06-10 04:32:02 +0900

    [ADD] 프로젝트 생성

---
+++ src/main/java/com/pickandlol/pickandlol/PickandlolApplication.java
@@ -0,0 +1,13 @@
+package com.pickandlol.pickandlol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickandlolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickandlolApplication.class, args);
+	}
+
+}
