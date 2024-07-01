e233a35dfefef4d24137e4af119669b52a922d9e
Lee <lsmin476@naver.com>
2024-07-01 15:45:03 +0900

    fix: save team api - team 저장시 oauthId 유일성 보장 로직 추가

--- src/main/java/com/pickandlol/pickandlol/Bean/SaveTeamBean.java
+++ src/main/java/com/pickandlol/pickandlol/Bean/SaveTeamBean.java
@@ -1,5 +1,6 @@
 package com.pickandlol.pickandlol.Bean;
 
+import com.pickandlol.pickandlol.Bean.Small.GetTeamByOauthIdDAOBean;
 import com.pickandlol.pickandlol.Bean.Small.SaveTeamDAOBean;
 import com.pickandlol.pickandlol.Model.RequestTeamSaveDTO;
 import com.pickandlol.pickandlol.Model.TeamDAO;
@@ -9,14 +10,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class SaveTeamBean {
 
+    GetTeamByOauthIdDAOBean getTeamByOauthIdDAOBean;
     SaveTeamDAOBean saveTeamDAOBean;
 
     @Autowired
-    public SaveTeamBean(SaveTeamDAOBean saveTeamDAOBean) {
+    public SaveTeamBean(GetTeamByOauthIdDAOBean getTeamByOauthIdDAOBean, SaveTeamDAOBean saveTeamDAOBean) {
+        this.getTeamByOauthIdDAOBean = getTeamByOauthIdDAOBean;
         this.saveTeamDAOBean = saveTeamDAOBean;
     }
 
     public String exec(RequestTeamSaveDTO requestTeamSaveDTO){
+
+        // 중복 제거
+        TeamDAO teamDAO1 = getTeamByOauthIdDAOBean.exec(requestTeamSaveDTO.getOauthId());
+        if (teamDAO1 != null) return "already exist";
 
         TeamDAO teamDAO = TeamDAO.builder()
                 .data(requestTeamSaveDTO.getData())
