af8edf1897566f5f39015d955a5bc822fa370901
Lee <lsmin476@naver.com>
2024-07-17 14:40:52 +0900

    fix: security update

--- src/main/java/com/pickandlol/pickandlol/Config/SecurityConfig.java
+++ src/main/java/com/pickandlol/pickandlol/Config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -22,7 +23,7 @@ public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf(AbstractHttpConfigurer::disable)
             .oauth2Login(oauth2 -> oauth2
                     .successHandler(successHandler) // 커스텀 성공 핸들러 사용
                     .userInfoEndpoint(userInfo -> userInfo
