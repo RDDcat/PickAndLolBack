1d6fbd12bc22a7c43840b390b6ff01d040ca6aef
Lee <lsmin476@naver.com>
2024-07-25 09:59:50 +0900

    fix: application-secert add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,5 +12,13 @@ CURRENT_PID=$(pgrep -f pickandlol-0.0.1-SNAPSHOT.jar)
  JAR_PATH="/home/ec2-user/cicd/build/libs/pickandlol-0.0.1-SNAPSHOT.jar"
  echo "jar path : $JAR_PATH"
  chmod +x $JAR_PATH
- nohup java -jar $JAR_PATH >> /home/ec2-user/cicd/deploy.log 2>> /home/ec2-user/cicd/deploy_err.log &
- echo "jar fild deploy success"
+
+# Ensure application-secret is in the right place
+SECRET_PATH="/home/ec2-user/cicd/src/main/resources/application-secret.properties"
+if [ ! -f "$SECRET_PATH" ]; then
+    echo "application-secret.properties not found!"
+    exit 1
+fi
+
+nohup java -jar $JAR_PATH --spring.config.additional-location=file:$SECRET_PATH >> /home/ec2-user/cicd/deploy.log 2>> /home/ec2-user/cicd/deploy_err.log &
+echo "jar file deploy success"
