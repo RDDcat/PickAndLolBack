89f81da61a72611f35b75be8f2abbef907b661f5
Lee <lsmin476@naver.com>
2024-08-19 17:08:09 +0900

    feat: add swagger docs to Club Controller

--- src/main/java/com/pickandlol/pickandlol/Controller/ClubController.java
+++ src/main/java/com/pickandlol/pickandlol/Controller/ClubController.java
@@ -2,6 +2,8 @@ package com.pickandlol.pickandlol.Controller;
 
 import com.pickandlol.pickandlol.Model.DTO.ResponseClubGetDTO;
 import com.pickandlol.pickandlol.Service.ClubService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/club")
+@Tag(name = "Club", description = "구단 관련 API")
 public class ClubController {
 
     ClubService clubService;
@@ -23,6 +26,7 @@ public class ClubController {
     }
 
     // 팀 전체 조회
+    @Operation(summary = "구단 전체 조회", description = "구단 순위를 조회를 위한 API")
     @GetMapping("/all")
     public List<ResponseClubGetDTO> getTeams(){
         return clubService.getTeams();
