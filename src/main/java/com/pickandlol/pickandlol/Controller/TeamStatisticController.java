65fa3509a524e09704367ace42006f7236c3ba94
Lee <lsmin476@naver.com>
2024-08-20 14:21:37 +0900

    feat: add swagger docs to Team Statistic Controller

--- src/main/java/com/pickandlol/pickandlol/Controller/TeamStatisticController.java
+++ src/main/java/com/pickandlol/pickandlol/Controller/TeamStatisticController.java
@@ -2,6 +2,8 @@ package com.pickandlol.pickandlol.Controller;
 
 import com.pickandlol.pickandlol.Model.DTO.ResponseTeamStatisticGetDTO;
 import com.pickandlol.pickandlol.Service.TeamStatisticService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/team/statistic")
+@Tag(name = "TeamStatistic", description = "유저 팀 통계 관련 API")
 public class TeamStatisticController {
 
     TeamStatisticService teamStatisticService;
@@ -20,12 +23,14 @@ public class TeamStatisticController {
     }
 
     // 팀 통계 저장
+    @Operation(summary = "유저 팀 통계 저장", description = "유저 팀 통계 저장 API")
     @PostMapping("")
     public void saveTeamStatistic(){
         teamStatisticService.saveTeamStatistic();
     }
 
     // 팀 통계 조회
+    @Operation(summary = "유저 팀 통계 전체 조회", description = "유저 팀 통계 전체 조회 API")
     @GetMapping("")
     public List<ResponseTeamStatisticGetDTO> getTeamStatistics(){
         return teamStatisticService.getTeamStatistics();
